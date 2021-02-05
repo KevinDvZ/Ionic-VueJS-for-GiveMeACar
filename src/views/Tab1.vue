9755b59329d0d83fcd6baaee246976df663760d3
Kevin DvZ <kevin.deveza@gmail.com>
2021-02-05 11:57:29 +0100

    firstCommit

---
+++ src/views/Tab1.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <periode-picker></periode-picker>
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import PeriodePicker from "@/components/periode-picker.vue";
+
+export default  {
+  name: 'Tab1',
+  components: {PeriodePicker, ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+</script>
