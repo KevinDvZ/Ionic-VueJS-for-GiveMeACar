db0a88f612330eda3fa097ba1a637911b0b58746
Kevin DvZ <kevin.deveza@gmail.com>
2021-02-11 17:07:51 +0100

    connexion AJAX complete !

--- src/components/TownPicker.vue
+++ src/components/TownPicker.vue
@@ -1,24 +1,49 @@
 <template>
   <div class="townText">
-    <ion-label>Choisissez votre ville :</ion-label>
-  <ion-item>
-    <ion-select interface="popover">
-      <ion-select-option value="nantes">Nantes</ion-select-option>
-      <ion-select-option value="brest">Brest</ion-select-option>
-      <ion-select-option value="morlaix">Morlaix</ion-select-option>
-      <ion-select-option value="pontivy">Pontivy</ion-select-option>
-      <ion-select-option value="rennes">Rennes</ion-select-option>
-      <ion-select-option value="angers">Angers</ion-select-option>
-      <ion-select-option value="laval">Laval</ion-select-option>
-      <ion-select-option value="laRocheSurYon">La Roche-sur-Yon</ion-select-option>
+     <ion-label>Choisissez votre ville :</ion-label>
+   <ion-item>
+    <ion-select interface="popover"
+    placeholder="Choisissez une agence"
+    :value="selectedOption"
+    @ionChange="selectedOption= $event.target.value">
+      <ion-select-option
+      class="ion-text-capitalize"
+      v-for="(agence, idAgence) in listeAgences"
+      :key="idAgence"
+      :value="agence.ville"
+      > {{agence.ville}} </ion-select-option>
     </ion-select>
   </ion-item>
   </div>
+
 </template>
 
 <script>
 export default {
   name: "TownPicker",
+  data: function() {
+return {
+    listeAgences: [],
+  };
+},
+  methods : {
+    fetchData () {
+      fetch(`http://warm-cove-08783.herokuapp.com/agences`)
+          .then(res => res.json())
+          .then(post =>  this.remplirTableau(post))
+    },
+    remplirTableau (item) {
+      this.listeAgences = item;
+
+    },
+    log(item){
+      console.log(item)
+    }
+  },
+  created(){
+    this.fetchData();
+
+  }
 };
 </script>
 
